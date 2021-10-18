090589bad5b1e3f651c502d1f60d019e48e23123
Kyosuke Takayama <loiseau@gmail.com>
2021-10-18 09:01:51 +0900

    Add support simple return statement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,6 +26,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.IfExpression:
 		return evalIfExpression(node)
 
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
+
 	// 式
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -49,6 +53,9 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	var result object.Object
 	for _, statement := range stmts {
 		result = Eval(statement)
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
 	}
 	return result
 }
