27fe98b6a3f461d7f2cf992867b01f75288b63f8
Kyosuke Takayama <loiseau@gmail.com>
2021-10-21 08:58:27 +0900

    Add more test case

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -120,7 +120,10 @@ func TestReturnStatement(t *testing.T) {
 		{"return 10; 9;", 10},
 		{"return 2 * 5; 9;", 10},
 		{"9; return 2 * 5; 9;", 10},
+		{"if (10 > 1) { return 10; }", 10},
 		{"if (10 > 1) { if (10 > 1) { return 10; } return 1; }", 10},
+		{"let f = fn(x) { return x; x + 10; }; f(10);", 10},
+		{"let f = fn(x) { let result = x + 10; return result; return 10; }; f(10);", 20},
 	}
 
 	for _, tt := range tests {
